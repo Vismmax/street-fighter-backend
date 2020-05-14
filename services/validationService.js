1152451a72814e97b8a5a6c36aef47dd0c7d8ca8
Vismmax <vismmax@gmail.com>
2020-05-14 20:50:07 +0300

    Add function removeExcessFields

--- services/validationService.js
+++ services/validationService.js
@@ -4,4 +4,15 @@ const validateId = (body) => {
     return !('id' in body);
 }
 
+const removeExcessFields = (body) => {
+    let validUser = {};
+    for (let key in body) {
+        if ((key in user) && key !== 'id') {
+            validUser[key] = body[key];
+        }
+    }
+    return validUser;
+}
+
 exports.validateId = validateId;
+exports.removeExcessFields = removeExcessFields;
