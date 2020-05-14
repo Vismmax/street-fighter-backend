f10e000435cd0c856b0290b88319c4a2499e0739
Vismmax <vismmax@gmail.com>
2020-05-14 21:20:29 +0300

    Add router /api/users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,8 +7,9 @@ const router = Router();
 
 // TODO: Implement route controllers for user
 
-router.post('/', (req, res, next) => {
+router.post('/', createUserValid, (req, res, next) => {
     console.log(req.body);
-}, createUserValid);
+    res.status(200).send(req.body);
+});
 
 module.exports = router;
