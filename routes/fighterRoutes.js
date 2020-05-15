d7f2c6341c53cc4340e9f3626ea363d7ad077904
Vismmax <vismmax@gmail.com>
2020-05-15 18:23:46 +0300

    Add pretty to router

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -1,11 +1,9 @@
-const { Router } = require('express');
+const {Router} = require('express');
 const FighterService = require('../services/fighterService');
-const { responseMiddleware } = require('../middlewares/response.middleware');
-const { createFighterValid, updateFighterValid } = require('../middlewares/fighter.validation.middleware');
+const {responseMiddleware} = require('../middlewares/response.middleware');
+const {createFighterValid, updateFighterValid} = require('../middlewares/fighter.validation.middleware');
 
 const router = Router();
-
-// TODO: Implement route controllers for fighter
 
 router.get('/', (req, res, next) => {
     try {
