6994b91f66a5deb6ec4a1ef01d07e1c8530e6c94
Vismmax <vismmax@gmail.com>
2020-05-15 08:40:41 +0300

    Add choice error to responseMiddleware

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,8 +1,9 @@
 const { createError } = require('../services/errorService');
 
 const responseMiddleware = (req, res, next) => {
+    let statusErr = res.statusErr || 400;
     if (res.err) {
-        res.status(404).send(createError(res.err.message));
+        res.status(statusErr).send(createError(res.err.message));
     } else {
         res.status(200).send(res.data);
     }
