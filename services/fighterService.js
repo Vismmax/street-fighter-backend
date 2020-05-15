5225f3a320f7d900aa07e72d80b05067a5aed07e
Vismmax <vismmax@gmail.com>
2020-05-15 15:15:18 +0300

    Add FighterService

--- services/fighterService.js
+++ services/fighterService.js
@@ -1,7 +1,35 @@
 const { FighterRepository } = require('../repositories/fighterRepository');
+const { removeExcessFields } = require('../services/validationFighterService');
 
 class FighterService {
     // TODO: Implement methods to work with fighters
+
+    search(search) {
+        const item = FighterRepository.getOne(search);
+        if(!item) {
+            return null;
+        }
+        return item;
+    }
+
+    getFighters() {
+        // const fighters = FighterRepository.getAll();
+        return FighterRepository.getAll();
+    }
+
+    create(fighterData) {
+        const data = removeExcessFields(fighterData);
+        try {
+            let user = FighterRepository.create(data);
+            if (!user) {
+                throw Error('User not saved');
+            }
+            return user;
+        }
+        catch (error) {
+            throw Error('User not saved');
+        }
+    }
 }
 
 module.exports = new FighterService();
