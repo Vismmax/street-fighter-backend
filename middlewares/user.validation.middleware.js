25e30aab965fcd8726c6eea26e5ca3d469e98bce
Vismmax <vismmax@gmail.com>
2020-05-15 09:43:40 +0300

    createUserValid return error

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -14,27 +14,26 @@ const createUserValid = (req, res, next) => {
     let existField = validateExistFields(req.body);
     let emptyFields = validateEmptyFields(req.body);
     if (!validateId(req.body)) {
-        res.status(400).send(createError(`User entity to create isn't valid. Exist field "id"`));
+        res.err = Error(`User entity to create isn't valid. Exist field "id"`);
+        res.err.status = 400;
     } else if (existField !== true) {
-        console.log(existField)
-        res.status(400).send(createError(`User entity to create isn't valid. No field "${existField}"`));
+        res.err = Error(`User entity to create isn't valid. No field "${existField}"`);
+        res.err.status = 400;
     } else if (emptyFields !== true) {
-        console.log(existField);
-        res.status(400).send(createError(`User entity to create isn't valid. Empty field ${emptyFields}`));
+        res.err = Error(`User entity to create isn't valid. Empty field ${emptyFields}`);
+        res.err.status = 400;
     } else if (!validateEmail(req.body.email)) {
-        console.log('email');
-        res.status(400).send(createError(`User entity to create isn't valid. Incorrect email`));
+        res.err = Error(`User entity to create isn't valid. Incorrect email`);
+        res.err.status = 400;
     } else if (!validatePhoneNumber(req.body.phoneNumber)) {
-        console.log('phone');
-        res.status(400).send(createError(`User entity to create isn't valid. Incorrect phone number`));
+        res.err = Error(`User entity to create isn't valid. Incorrect phone number`);
+        res.err.status = 400;
     } else if (!validatePassword(req.body.password)) {
-        console.log('pass');
-        res.status(400).send(createError(`User entity to create isn't valid. Incorrect password`));
-    } else {
-        next();
+        res.err = Error(`User entity to create isn't valid. Incorrect password`);
+        res.err.status = 400;
     }
 
-    // next();
+    next();
 }
 
 const updateUserValid = (req, res, next) => {
