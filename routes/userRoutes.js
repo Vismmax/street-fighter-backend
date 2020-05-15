0e8daae2d4e2a448e3929bc8049f68177cf920de
Vismmax <vismmax@gmail.com>
2020-05-15 08:39:38 +0300

    Add responseMiddleware to userRouter

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,20 +2,21 @@ const { Router } = require('express');
 const UserService = require('../services/userService');
 const { createUserValid, updateUserValid } = require('../middlewares/user.validation.middleware');
 const { responseMiddleware } = require('../middlewares/response.middleware');
-const { createError } = require('../services/errorService');
 
 const router = Router();
 
 // TODO: Implement route controllers for user
 
 router.post('/', createUserValid, (req, res, next) => {
-    let data = UserService.create(req.body);
-    if (data) {
-        res.status(200).send(data);
+    try {
+        let data = UserService.create(req.body);
+        res.data = data;
+    } catch (err) {
+        res.err = err;
+        res.statusErr = 400;
+    } finally {
+        next();
     }
-    else {
-        res.status(400).send(createError('Data not saved'));
-    }
-});
+}, responseMiddleware);
 
 module.exports = router;
