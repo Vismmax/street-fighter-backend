ffac756a7e85bebe0c55b6cf0c4418174e841c49
Vismmax <vismmax@gmail.com>
2020-05-16 08:52:57 +0300

    Add pretty to userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,9 @@
-const { Router } = require('express');
+const {Router} = require('express');
 const UserService = require('../services/userService');
-const { createUserValid, updateUserValid } = require('../middlewares/user.validation.middleware');
-const { responseMiddleware } = require('../middlewares/response.middleware');
+const {createUserValid, updateUserValid} = require('../middlewares/user.validation.middleware');
+const {responseMiddleware} = require('../middlewares/response.middleware');
 
 const router = Router();
-
-// TODO: Implement route controllers for user
 
 router.get('/', (req, res, next) => {
     try {
