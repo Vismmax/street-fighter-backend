c137e1b552b10ea9aa79fb467fd41d42776d391f
Vismmax <vismmax@gmail.com>
2020-05-16 09:14:48 +0300

    Edit error messages

--- middlewares/fighter.validation.middleware.js
+++ middlewares/fighter.validation.middleware.js
@@ -8,12 +8,12 @@ const { validatePower } = require('../services/validationFighterService');
 const { validateDefense } = require('../services/validationFighterService');
 
 const createFighterValid = (req, res, next) => {
-    let err = validationFighter(req.body);
+    let err = validationFighter(req.body, 'create');
     if (err) {
         res.err = err;
         res.err.status = 400;
     } else if (FighterService.search({name: req.body.name})) {
-        res.err = Error(`User entity to create isn't valid. Fighter exist`);
+        res.err = Error(`Fighter entity to create isn't valid. Fighter exist`);
         res.err.status = 400;
     }
 
@@ -21,31 +21,31 @@ const createFighterValid = (req, res, next) => {
 }
 
 const updateFighterValid = (req, res, next) => {
-    let err = validationFighter(req.body);
+    let err = validationFighter(req.body, 'update');
     if (err) {
         res.err = err;
         res.err.status = 400;
     } else if (!FighterService.search({id: req.params.id})) {
-        res.err = Error(`User entity to create isn't valid. Fighter not exist`);
+        res.err = Error(`Fighter entity to update isn't valid. Fighter not exist`);
         res.err.status = 400;
     }
 
     next();
 }
 
-function validationFighter(fighter) {
+function validationFighter(fighter, action) {
     let existField = validateExistFields(fighter);
     let emptyFields = validateEmptyFields(fighter);
     if (!validateId(fighter)) {
-        return Error(`User entity to create isn't valid. Exist field "id"`);
+        return Error(`Fighter entity to ${action} isn't valid. Exist field "id"`);
     } else if (existField !== true) {
-        return Error(`User entity to create isn't valid. No field "${existField}"`);
+        return Error(`Fighter entity to ${action} isn't valid. No field "${existField}"`);
     } else if (emptyFields !== true) {
-        return Error(`User entity to create isn't valid. Empty field ${emptyFields}`);
+        return Error(`Fighter entity to ${action} isn't valid. Empty field ${emptyFields}`);
     } else if (!validatePower(fighter.power)) {
-        return Error(`User entity to create isn't valid. Incorrect power`);
+        return Error(`Fighter entity to ${action} isn't valid. Incorrect power`);
     } else if (!validateDefense(fighter.defense)) {
-        return Error(`User entity to create isn't valid. Incorrect defense`);
+        return Error(`Fighter entity to ${action} isn't valid. Incorrect defense`);
     }
     return false;
 }
