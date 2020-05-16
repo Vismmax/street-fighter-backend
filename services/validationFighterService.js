e3ee30bac16391b2907f3428c6134e2249eabc31
Vismmax <vismmax@gmail.com>
2020-05-16 12:18:03 +0300

    Refactoring validationFighterService.js

--- services/validationFighterService.js
+++ services/validationFighterService.js
@@ -1,46 +1,20 @@
 const {fighter} = require('../models/fighter');
+const {BaseValidationService} = require('../services/baseValidationService');
 
-const validateId = (body) => {
-    return !('id' in body);
-}
-
-const removeExcessFields = (body) => {
-    let validUser = {};
-    for (let key in body) {
-        if ((key in fighter) && key !== 'id') {
-            validUser[key] = body[key];
-        }
+class FighterValidationService extends BaseValidationService {
+    constructor() {
+        super(fighter);
     }
-    return validUser;
-}
 
-const validateExistFields = (body) => {
-    for (let key in fighter) {
-        if (!(key in body) && key !== 'id') return key;
+    validatePower(power) {
+        let pw = parseInt(power);
+        return !(pw < 1 || pw > 100);
     }
-    return true;
-}
 
-const validateEmptyFields = (body) => {
-    for (let key in body) {
-        if (!body[key]) return key;
+    validateDefense(defense) {
+        let def = parseInt(defense);
+        return !(def < 0 || def > 100);
     }
-    return true;
 }
 
-const validatePower = (power) => {
-    let pw = parseInt(power);
-    return !(pw < 1 || pw > 100);
-}
-
-const validateDefense = (defense) => {
-    let def = parseInt(defense);
-    return !(def < 0 || def > 100);
-}
-
-exports.validateId = validateId;
-exports.removeExcessFields = removeExcessFields;
-exports.validateExistFields = validateExistFields;
-exports.validateEmptyFields = validateEmptyFields;
-exports.validatePower = validatePower;
-exports.validateDefense = validateDefense;
+exports.FighterValidationService = new FighterValidationService();
