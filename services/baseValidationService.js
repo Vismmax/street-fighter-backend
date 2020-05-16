37f01a0a0ec5d33a5add138611e5589714ead13a
Vismmax <vismmax@gmail.com>
2020-05-16 12:14:30 +0300

    Add baseValidationService.js

---
+++ services/baseValidationService.js
@@ -0,0 +1,35 @@
+class BaseValidationService {
+    constructor(model) {
+        this.model = model;
+    }
+
+    validateId(body) {
+        return !('id' in body);
+    }
+
+    validateExistFields(body) {
+        for (let key in this.model) {
+            if (!(key in body) && key !== 'id') return key;
+        }
+        return true;
+    }
+
+    validateEmptyFields(body) {
+        for (let key in body) {
+            if (!body[key]) return key;
+        }
+        return true;
+    }
+
+    removeExcessFields = (body) => {
+        let validUser = {};
+        for (let key in body) {
+            if ((key in this.model) && key !== 'id') {
+                validUser[key] = body[key];
+            }
+        }
+        return validUser;
+    }
+}
+
+exports.BaseValidationService = BaseValidationService;
